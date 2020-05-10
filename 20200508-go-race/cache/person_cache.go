a39a3b26ba90c5a0428ddc556941158fe561d4ec
yinhang.sun <yinhang.sun@shopee.com>
2020-05-11 07:44:48 +0800

    update string length

--- 20200508-go-race/cache/person_cache.go
+++ 20200508-go-race/cache/person_cache.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+const Max = uint64(1)
 
 type PersonCache struct {
 	c *gocache.Cache
@@ -21,14 +22,16 @@ func NewPersonCache() *PersonCache {
 }
 
 func (p *PersonCache) load() {
-	r :=  rand.Uint64() % 100
-	key := strconv.FormatUint(r,10)
-	newOne := &pb.Person{
-		Id:                   proto.Uint64(10),
-		Name:                 proto.String("init"),
-		Age:                  proto.Uint32(rand.Uint32()),
+	for i:=uint64(0); i < Max; i++{
+		r :=  i % 100
+		key := strconv.FormatUint(r,10)
+		newOne := &pb.Person{
+			Id:                   proto.Uint64(10),
+			Name:                 proto.String("init"),
+			Age:                  proto.Uint32(rand.Uint32()),
+		}
+		p.c.Set(key, newOne, time.Minute)
 	}
-	p.c.Set(key, newOne, time.Minute)
 }
 
 func (p *PersonCache) Get(key string) (*pb.Person,bool) {
--- 20200508-go-race/main.go
+++ 20200508-go-race/main.go
@@ -11,43 +11,61 @@ import (
 	"syscall"
 )
 
+const Concurrency = 10
+const MaxLen = 100
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterLength = len(letterBytes)
+
+func RandLengthString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
 func main() {
 	c := cache.NewPersonCache()
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	go func () {
-		for {
-			select {
-			case <-ch:
-				break
-			default:
-				r := rand.Uint64() % 100
-				key := strconv.FormatUint(r, 10)
-				p, ok := c.Get(key)
-				if !ok {
-					continue
+	for i:=0;i<Concurrency;i++ {
+		go func() {
+			for {
+				select {
+				case <-ch:
+					break
+				default:
+					r := rand.Uint64() % cache.Max
+					key := strconv.FormatUint(r, 10)
+					p, ok := c.Get(key)
+					if !ok {
+						continue
+					}
+					p.Name = proto.String(RandLengthString(rand.Int()%letterLength))
 				}
-				p.Name = proto.String("reading....")
 			}
-		}
-	}()
+		}()
+	}
 
-	go func() {
-		for {
-			select {
-			case <-ch:
-				break
-			default:
-				r :=  rand.Uint64() % 100
-				key := strconv.FormatUint(r,10)
-				p,ok := c.Get(key)
-				if !ok {
-					continue
+	for i:=0;i<Concurrency;i++{
+		go func() {
+			for {
+				select {
+				case <-ch:
+					break
+				default:
+					r :=  rand.Uint64() % cache.Max
+					key := strconv.FormatUint(r,10)
+					p,ok := c.Get(key)
+					if !ok {
+						continue
+					}
+					proto.Marshal(p)
 				}
-				proto.Marshal(p)
 			}
-		}
-	}()
+		}()
+	}
 
 	fmt.Println(<-ch)
 }
